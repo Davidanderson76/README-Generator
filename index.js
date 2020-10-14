be0c8478a59704db91cbbcf883735a5e114c630e
David Anderson <david.anderson1993@gmail.com>
2020-10-14 17:47:59 -0500

    updating js file with questions added

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
 const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
+const generateMarkDown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Give a brief description of your project"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What kind of license will your project have?",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What command should be used to install the dependencies?",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What command should run to preform a test?",
+        default: "npm test"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What does the user need to know about this repo?"
+    },
+    {
+        input: "input",
+        name: "contributing",
+        message: "What does the user need to know about contributing to the repo?",
+    }
 ];
 
 // function to write README file
